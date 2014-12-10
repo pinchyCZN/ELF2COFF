e83f537ae070b3f0c7f2201dea76c960f5708470
pinchyCZN <pinchyCZN@gmail.com>
2014-12-10 14:41:24 -0600

    updates

--- elf2coff.c
+++ elf2coff.c
@@ -15,19 +15,30 @@ typedef __int64 int64;
 
 #include "elf.h"
 #include "coff.h"
+
+const int ELF_HEADER_OFFSETS[20][2]={ //offset,size
+{0,4},{4,1},{5,1},{6,1},{7,1},{8,4},{12,4},{16,2},{18,2},{20,4},{24,4},{28,4},{32,4},{36,4},{40,2},{42,2},{44,2},{46,2},{48,2},{50,2}
+};
 const char *ELF_HEADER_NAMES[20]={
 "e_ident","e_bitness","e_endian","e_elfver","e_reserved0","e_reserved1","e_reserved2","e_type","e_machine","e_version","e_entry","e_phoff","e_shoff","e_flags","e_ehsize","e_phentsize","e_phnum","e_shentsize","e_shnum","e_shstrndx"
 };
 enum {e_ident=0,e_bitness,e_endian,e_elfver,e_reserved0,e_reserved1,e_reserved2,e_type,e_machine,e_version,e_entry,e_phoff,e_shoff,e_flags,e_ehsize,e_phentsize,e_phnum,e_shentsize,e_shnum,e_shstrndx};
+
+const int ELF_SHEADER_OFFSETS[10][2]={ //offset,size
+{0,4},{4,4},{8,4},{12,4},{16,4},{20,4},{24,4},{28,4},{32,4},{36,4}
+};
+const char *ELF_SHEADER_NAMES[10]={
+"sh_name","sh_type","sh_flags","sh_addr","sh_offset","sh_size","sh_link","sh_info","sh_addralign","sh_entsize"
+};
+enum {sh_name=0,sh_type,sh_flags,sh_addr,sh_offset,sh_size,sh_link,sh_info,sh_addralign,sh_entsize};
+
+
 
 #define MEMBER_OFFSET 0
 #define MEMBER_SIZE 1
 #define B_ENDIAN ELFDATA2MSB
 #define L_ENDIAN ELFDATA2LSB
 
-const int ELF_HEADER_OFFSETS[20][2]={ //offset,size
-{0,4},{4,1},{5,1},{6,1},{7,1},{8,4},{12,4},{16,2},{18,2},{20,4},{24,4},{28,4},{32,4},{36,4},{40,2},{42,2},{44,2},{46,2},{48,2},{50,2}
-};
 int get_32bit(unsigned char *buf,int endian)
 {
 	int result=0;
@@ -77,9 +88,14 @@ int get_header_val(int member,unsigned char *buf,int buf_size,int *result)
 {
 	int endian,offset,size;
 	if(buf_size<sizeof(struct ELF32_HEADER)){
-		printf("size error getting %s\n",ELF_HEADER_NAMES[member]);
-		return 0;
+		printf("buffer too small, error getting %s\n",ELF_HEADER_NAMES[member]);
+		return FALSE;
 	}
+	if(ELFCLASS32!=buf[ELF_HEADER_OFFSETS[e_bitness][MEMBER_OFFSET]]){
+		printf("only 32bit elf supported\n");
+		return FALSE;
+	}
+	
 	offset=ELF_HEADER_OFFSETS[member][MEMBER_OFFSET];
 	size=ELF_HEADER_OFFSETS[member][MEMBER_SIZE];
 	endian=buf[ELF_HEADER_OFFSETS[e_endian][MEMBER_OFFSET]];
@@ -90,18 +106,52 @@ int get_header_val(int member,unsigned char *buf,int buf_size,int *result)
 	return FALSE;
 }
 
-
+int get_sheader_val(int member,int index,unsigned char *buf,int buf_size,int *result)
+{
+	int sect_offset,sect_count,sect_size,endian;
+	if(get_header_val(e_endian,buf,buf_size,&endian)
+		&& get_header_val(e_shoff,buf,buf_size,&sect_offset)
+		&& get_header_val(e_shnum,buf,buf_size,&sect_count)
+		&& get_header_val(e_shentsize,buf,buf_size,&sect_size)){
+		if(index>=sect_count){
+			printf("section count out of range\n");
+		}else{
+			int offset,size;
+			offset=ELF_SHEADER_OFFSETS[member][MEMBER_OFFSET];
+			size=ELF_SHEADER_OFFSETS[member][MEMBER_SIZE];
+			if(result){
+				char *data=buf+sect_offset+index*sect_size;
+				if((data+size+offset)>(buf+buf_size)){
+					printf("section header offset out of range\n");
+				}else{
+					*result=get_data(data,offset,size,endian);
+					return TRUE;
+				}
+			}
+		}
+	}
+	return FALSE;
+}
 int dump_elf(unsigned char *buf,int len)
 {
 	if(buf && len>0){
-		int i,offset;
+		int i,sect_count;
 		for(i=0;i<sizeof(ELF_HEADER_NAMES)/sizeof(char *);i++){
 			int val=0;
 			if(get_header_val(i,buf,len,&val))
 				printf("%-11s=0x%08X\n",ELF_HEADER_NAMES[i],val);
 		}
-		if(get_header_val(e_shoff,buf,len,&offset)){
-			
+		if(get_header_val(e_shnum,buf,len,&sect_count)){
+			int j;
+			for(j=0;j<sect_count;j++){
+				printf("---section %i-----\n",j);
+				for(i=0;i<sizeof(ELF_SHEADER_NAMES)/sizeof(char *);i++){
+					int val=0;
+					if(get_sheader_val(i,j,buf,len,&val)){
+						printf("%-12s=0x%08X\n",ELF_SHEADER_NAMES[i],val);
+					}
+				}
+			}
 		}
 	}
 }
@@ -158,6 +208,9 @@ int main(int argc,char **argv)
 		exit(0);
 		break;
 	}
-	getch();
+	if(getenv("PROMPT")==0){
+		printf("press any key\n");
+		getch();
+	}
 	return 0;
 }
